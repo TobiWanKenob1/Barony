84a2294b705421c7b261d47b2719c4402ee1fc33
TobiWanKenob1 <tobiaseberlein@icloud.com>
2026-09-12 03:31:55 +0200

    forgot version

--- src/ui/MainMenu.cpp
+++ src/ui/MainMenu.cpp
@@ -28702,7 +28702,7 @@ failed:
 	}
 
 	// mod add: executable version check
-	static constexpr const char* KNIGHTLY_EXE_VERSION = "3.00";
+	static constexpr const char* KNIGHTLY_EXE_VERSION = "3.01";
 
 	static int knightlyVersionNumber(const std::string& version)
 	{
